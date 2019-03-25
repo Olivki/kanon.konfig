aaee83be0c690390247c2f6eaf4345a3779a56db
Oliver Berg <oliver@berg.moe>
2019-03-25 16:55:39 +0100

    Increase build version
    Signed-off-by: Oliver Berg <oliver@berg.moe>

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 // Project Specific Variables
 project.group = "moe.kanon.konfig"
 project.description = "A type-safe configuration system for Kotlin."
-project.version = "1.1.2"
+project.version = "1.2.0"
 val artifactName = "kanon.konfig"
 val gitUrl = "https://gitlab.com/kanondev/kanon-konfig"
 
